e8e6d53b436b25abcf3796402dcc9178933d5613
nhiemngo <nngo@colgate.edu>
2018-03-25 17:54:38 -0400

    added more features

--- README.md
+++ README.md
@@ -4,16 +4,32 @@ Creator: Nhiem Ngo.
 
 This project used [Create React Native App](https://github.com/react-community/create-react-native-app).
 
-## Task list:
+## Features:
 - [X] User interface
 
 - [x] Phone number validation
 
 - [x] Send SMS with the Twilio API
 
+**Additional feature:**
+
+- [x] User can input the text content they want to send
+
 ## Interface:
 
-![Interface](https://i.imgur.com/uc2PRJ1.png)
+![Interface](./Screenshot_20180325-174328.png)
+
+## Current issue:
+
+I receive error 400 BAD REQUEST from the server, even after I successfully authorize the HTTP request. Somehow, the body of the request is not received. I found a lot of discussion on this problem online, but there doesn't seem to be a universal, acceptable solution to this:
+
+https://github.com/matthew-andrews/isomorphic-fetch/issues/34
+
+https://github.com/github/fetch/issues/323
+
+https://stackoverflow.com/questions/39565706/post-request-with-fetch-api
+
+The function that makes the request is send(), located on line 15 in Main.js
 
 
 ## References:
@@ -21,9 +37,5 @@ This project used [Create React Native App](https://github.com/react-community/c
 https://www.twilio.com/docs/api/messaging
 
 https://facebook.github.io/react-native/
-
-https://github.com/matthew-andrews/isomorphic-fetch/issues/34
-
-https://stackoverflow.com/questions/31473420/make-an-http-post-authentication-basic-request-using-javascript
 
 https://stackoverflow.com/questions/34815853/react-native-fetch-and-basic-authentication
--- app/components/Main.js
+++ app/components/Main.js
@@ -7,12 +7,11 @@ export default class Main extends React.Component {
 
   constructor(props) {
       super(props);
-      this.state = {text: '+'};
+      this.state = {phoneNumber: '', textBody: ''};
     }
 
   //use the Twilio REST API to send message to the designated number
-  //I follow the documentation here: https://www.twilio.com/docs/api/rest
-  send(toNumber)  {
+  send(toNumber, content)  {
     var headers = new Headers()
     headers.append('Accept', 'application/json')
     headers.append('Content-Type', 'application/json')
@@ -26,7 +25,7 @@ export default class Main extends React.Component {
       //originally, I cannot authorize the request just by setting headers. I also tried to put the credentials directly in the url, but that did not work
       //I discovered the solution at https://stackoverflow.com/questions/34815853/react-native-fetch-and-basic-authentication
       headers: headers,
-      //for some reason, the body cannot be sent. I have already authorized the request but always get 400: Bad request from the server
+      //for some reason, the body of the request cannot be sent. I have already authorized the request but always get 400: Bad request from the server
       body: JSON.stringify({
         'To': '+13158257415',
         'From': '+12147616710',
@@ -35,9 +34,9 @@ export default class Main extends React.Component {
     })
     .then((response) => {
       temp = response.status.toString()
-      //Let the user know the result of their request
+      //let the user know the result of their request
       if (temp == '400') {
-        Alert.alert('Error ' + temp + ': the SMS cannot be sent to ' + toNumber)
+        Alert.alert('Error ' + temp + ': the SMS cannot be sent to ' + toNumber + '\nBody: ' + content)
       }
       else if (temp == '201'){
         Alert.alert('SMS sent to ' + toNumber)
@@ -49,7 +48,7 @@ export default class Main extends React.Component {
 
   }
 
-  //returns only the numbers in the input value, preceded by '+'
+  //returns only the numbers in the input value, preceded by '+'. Used for validating the phone number.
   validateInput(inputValue) {
     testValue = "0123456789"
     returnValue = ""
@@ -61,10 +60,13 @@ export default class Main extends React.Component {
     return ('+' + returnValue)
   }
 
-  //use validateInput() on the input data, then passing it to send()
+  //validate the input data then pass it to send()
   onPressButton() {
-      k = this.validateInput(this.state.text)
-      this.send(k)
+      num = this.validateInput(this.state.phoneNumber)
+      body = "Testing"
+      if (this.state.textBody != "")
+        body = this.state.textBody
+      this.send(num, body)
       }
 
   //user interface:
@@ -74,19 +76,27 @@ export default class Main extends React.Component {
         <View><Text style={styles.titleText}>Send SMS</Text></View>
           <View>
             <TextInput
-              style={styles.textbox}
-              value = {this.state.text}
-              onChangeText={(text) => this.setState({text})}
+              style={styles.numberBox}
+              value = {this.state.phoneNumber}
+              placeholder = "# (With country code)"
+              onChangeText={(phoneNumber) => this.setState({phoneNumber})}
             />
           </View>
 
           <View>
-            <Text style={styles.subtitleText}>(Please include country code)</Text>
+            <TextInput
+              style={styles.bodyBox}
+              maxLength = {120}
+              multiline = {true}
+              placeholder = "Body (Optional)"
+              value = {this.state.textBody}
+              onChangeText={(textBody) => this.setState({textBody})}
+            />
           </View>
 
           <View style={styles.buttonContainer}>
             <Button
-              onPress={(text) => this.onPressButton()}
+              onPress={(phoneNumber) => this.onPressButton()}
               title="Send"
               color = '#111f4d'
             />
@@ -100,7 +110,7 @@ export default class Main extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 5,
+    flex: 1,
     backgroundColor: '#f2f4f7',
     flexDirection: 'column',
     justifyContent: 'center',
@@ -113,22 +123,28 @@ const styles = StyleSheet.create({
     color: '#e43a19',
   },
 
-  subtitleText: {
-    fontSize: 10,
-  },
-
-  textbox: {
-    fontSize:20,
-    width:180,
+  numberBox: {
+    fontSize:17,
+    width:160,
+    paddingLeft: 5,
+    paddingRight: 5,
     paddingBottom:10,
     paddingTop:10,
-    paddingLeft:10,
-    paddingRight:10,
+    color: '#111f4d',
+  },
+
+  bodyBox: {
+    fontSize:17,
+    width:160,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingBottom:10,
+    paddingTop:10,
     color: '#111f4d',
   },
 
   buttonContainer: {
-    margin: 20
+    margin: 5
   }
 
 });
